da287961f6395b6b974e3f1be747027d3ba44cbf
lv7948135-collab <lv7948135@gmail.com>
2026-06-19 19:52:06 +0300

    Update states.py

--- bot/states.py
+++ bot/states.py
@@ -1,7 +1,5 @@
 from aiogram.fsm.state import State, StatesGroup
 
-
-class Survey(StatesGroup):
-    waiting_for_calc = State()  # ждём вставку результата калькулятора
-    q1_rating = State()         # вопрос 1: рейтинг карточки
-    q2_problem = State()        # вопрос 2: главная проблема
+class UserStates(StatesGroup):
+    waiting_consent = State()
+    waiting_question = State()
